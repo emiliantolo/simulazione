01a66f9ab2bcaecbdd2208540fcb8ebd70b20d61
emiliantolo <emiliantolo@gmail.com>
2018-12-05 12:30:11 +0100

    tests - fetch

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express=require ("express");
 const app=express();
 const PORT = process.env.PORT || 3000;
-
+const bodyparser = require ("body-parser");
 const string_square=require("./string_square");
+
+app.use(bodyparser.json());
 
 app.get('/', (req, res) => res.send("Hello World"));
 
 app.get('/square', (req, res) => res.send(
-    string_square(req.query.string)+""
+    res.status(200).send(string_square(req.query.string)+"")
 ));
 
 
--- string_square.js
+++ string_square.js
@@ -7,7 +7,7 @@ let string_square=function(s){
 	p=l*l;
     }
     
-    return p;
+    return {result: p};
 }
 
 module.exports=string_square;
