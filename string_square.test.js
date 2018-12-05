0c642d1ae756bf20457811660b40733778add9b0
emiliantolo <emiliantolo@gmail.com>
2018-12-05 12:47:09 +0100

    local function tests

--- string_square.test.js
+++ string_square.test.js
@@ -2,35 +2,35 @@ const string_square=require("./string_square");
 const fetch=require("node-fetch");
 
 test("Void test", () => {
-    expect (string_square()).toBe(-1)
+    expect (string_square().result).toBe(-1)
 });
 
 test("Nomal test", () => {
-    expect (string_square("abc")).toBe(9)
+    expect (string_square("abc").result).toBe(9)
 });
 
 test("Number test", () => {
-    expect (string_square(-5)).toBe(-1)
+    expect (string_square(-5).result).toBe(-1)
 });
 
 test("Empty String test", () => {
-    expect (string_square("")).toBe(0)
+    expect (string_square("").result).toBe(0)
 });
 
 test("Empty String test", () => {
-    expect (string_square("")).toBe(0)
+    expect (string_square("").result).toBe(0)
 });
 
 test("null test", () => {
-    expect (string_square(null)).toBe(-1)
+    expect (string_square(null).result).toBe(-1)
 });
 
 test("undefined test", () => {
-    expect (string_square(undefined)).toBe(-1)
+    expect (string_square(undefined).result).toBe(-1)
 });
 
 test("object test", () => {
-    expect (string_square([0, "abc"])).toBe(-1)
+    expect (string_square([0, "abc"]).result).toBe(-1)
 });
 
 test("fetch test Normal String", () => {
